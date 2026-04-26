171b13427f60203940b6faca56488c3d9d3e7047
inhwoi7 <inhwoi7@gmail.com>
2026-04-26 23:43:22 +0900

    export const revalidate = 0; 삽입

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import {
 } from "@/components/icons";
 import { getArticlePosts, getFeaturedQuote } from "@/lib/notion";
 
+export const revalidate = 0;
+
 type ServiceItem = {
   title: string;
   description: string;
