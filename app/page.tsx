dc8e8d5cec941ea9894ca09e99058dfe450846f0
inhwoi7 <inhwoi7@gmail.com>
2026-04-28 07:26:54 +0900

    fix: notion import 경로 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { ArrowRightIcon, ServiceIcon, SunLogo } from "@/components/icons";
-import { getArticlePosts, getFeaturedQuote } from "../../lib/notion";
+import { getArticlePosts, getFeaturedQuote } from "@/lib/notion";
 
 export const revalidate = 0;
 
