9bf3fa6bc4dbe867714ca6f82a83dbe81a419ff3
inhwoi7 <inhwoi7@gmail.com>
2026-04-27 23:58:59 +0900

    fix: Footer 텍스트 색상 진하게 수정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,19 +16,19 @@ export default function Footer() {
         flexWrap: "wrap",
         marginBottom: "12px",
       }}>
-        <Link href="/about" style={{ color: "#8c7b6b", fontSize: "13px", textDecoration: "none" }}>
+        <Link href="/about" style={{ color: "#6b5040", fontSize: "13px", textDecoration: "none" }}>
           서비스 소개
         </Link>
         <span style={{ color: "#c8b8a8", fontSize: "13px" }}>|</span>
-        <Link href="/privacy" style={{ color: "#8c7b6b", fontSize: "13px", textDecoration: "none" }}>
+        <Link href="/privacy" style={{ color: "#6b5040", fontSize: "13px", textDecoration: "none" }}>
           개인정보처리방침
         </Link>
         <span style={{ color: "#c8b8a8", fontSize: "13px" }}>|</span>
-        <Link href="/terms" style={{ color: "#8c7b6b", fontSize: "13px", textDecoration: "none" }}>
+        <Link href="/terms" style={{ color: "#6b5040", fontSize: "13px", textDecoration: "none" }}>
           이용약관
         </Link>
       </nav>
-      <p style={{ color: "#b0a090", fontSize: "12px", margin: 0 }}>
+      <p style={{ color: "#8c7b6b", fontSize: "12px", margin: 0 }}>
         © 2026 다상담(Dasangdam). All rights reserved.
       </p>
     </footer>
