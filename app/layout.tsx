6deaaa56565030003ab2a622a78699d30cc77468
inhwoi7 <inhwoi7@gmail.com>
2026-05-04 15:26:22 +0900

    위치 추적 지우고 원복

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Script from "next/script";
-import TrackingProvider from "@/components/TrackingProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -45,7 +44,6 @@ export default function RootLayout({
         </Script>
       </head>
       <body className="min-h-full flex flex-col">
-        <TrackingProvider />
         {children}
         <Footer />
       </body>
