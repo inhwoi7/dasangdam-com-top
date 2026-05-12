b9c4aeee0dff74cc357cedb640a3da1268dd38a7
inhwoi7 <inhwoi7@gmail.com>
2026-05-12 22:21:24 +0900

    fix: revert next.config turbo option

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./i18n.ts");
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: undefined,
-  },
   images: {
     remotePatterns: [
       {
